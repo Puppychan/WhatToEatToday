d001a6ff8646d27a34313852d39f1ff419cf4a75
Matsuri <nhungmaitran1412@gmail.com>
2022-07-19 19:56:49 +0700

    Initial Commit

---
+++ WhatToEatToday/WhatToEatTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToEatTodayApp.swift
+//  WhatToEatToday
+//
+//  Created by Nhung Tran on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatToEatTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
