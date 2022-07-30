9d9fd90bd5a1fc78290d5293110dc083eaf2772c
Matsuri <nhungmaitran1412@gmail.com>
2022-07-31 00:48:30 +0700

    Add image scrolling effect on restaurant view

--- WhatToEatToday/Views/Home/DetailView/RestaurantDetailView.swift
+++ WhatToEatToday/Views/Home/DetailView/RestaurantDetailView.swift
@@ -5,14 +5,54 @@
 //  Created by Nhung Tran on 26/07/2022.
 //
 // ref: https://stackoverflow.com/questions/59158476/how-to-have-text-in-shapes-in-swiftui
+//https://blckbirds.com/post/stretchy-header-and-parallax-scrolling-in-swiftui/
 
 
 import SwiftUI
 
+//struct GeometryGetter: View {
+//    var rest: Restaurant
+//    @Binding var imageScale: CGFloat
+//    var body: some View {
+//        GeometryReader { geo in
+//            ZStack {
+//
+//                if geo.frame(in: .global).minY <= 0 {
+//                    // scroll down outside the screen
+//                    Image("\(rest.name)-bck")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fill)
+//                        .offset(y: geo.frame(in: .global).minY/9)
+//                        .clipped()
+//
+//                }
+//                else {
+//                    Image("\(rest.name)-bck")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fill)
+//                        .frame(width: geo.size.width, height: geo.size.height)
+//                        .scaleEffect(imageScale) // change scale of image
+//                        .animation(.default.delay(1.3), value: imageScale)
+//                        .clipped()
+////                                    .offset(y: -geo.frame(in: .global).minY)
+//                }
+//            }
+//        }
+//        .frame(minHeight: 500)
+//    }
+//
+//    func makeView(geo: GeometryProxy) -> some View {
+//        DispatchQueue.main.async {
+//                    self.rest = geo.frame(in: .global)
+//                }
+//
+//                return Rectangle().fill(Color.clear)
+//    }
+//}
 
 struct RestaurantDetailView: View {
     var rest: Restaurant
-    @State var imageHeight : CGFloat = 0
+    @State var imageHeight: CGFloat = 0
 //    @State var isFoodDetailShowing = false
     @State var imageScale = 1.0
     @EnvironmentObject var model: RestaurantModel
@@ -24,14 +64,35 @@ struct RestaurantDetailView: View {
                 // MARK: - Restaurant Info
                 VStack {
                     // MARK: restaurant cover image
-                    Image("\(rest.name)-bck")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                    // for changing scale of image when scroll
-                        .scaleEffect(imageScale)
-                        .animation(.default.delay(1.3), value: imageScale)
-                        .clipped()
-                    
+                    GeometryReader { geo in
+                        ZStack {
+
+                            if geo.frame(in: .global).minY <= 0 {
+                                // scroll down outside the screen -> move image up
+                                Image("\(rest.name)-bck")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .offset(y: geo.frame(in: .global).minY/9)
+                                    .clipped()
+                                
+                            }
+                            else {
+                                // scroll up -> increase image size + move image down
+                                Image("\(rest.name)-bck")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .scaleEffect(1 + geo.frame(in: .global).minY / 500) // change scale of image
+                                    .animation(.default.delay(1.3), value: imageScale)
+                                    .clipped()
+                                                .offset(y: -geo.frame(in: .global).minY)
+                            }
+                        }
+
+                    }
+                    .frame(minHeight: 500)
+
+
+
                     VStack(alignment: .leading, spacing: 13) {
                         // MARK: restaurant name
                         Text(rest.name)
@@ -48,15 +109,15 @@ struct RestaurantDetailView: View {
                             Text(rest.address)
                                 .foregroundColor(Color("RestDetailAddColor"))
                                 .font(.headline)
-                            
+
                             Spacer()
-                            
+
                             // MARK: restaurant rating
                             RatingView(rest: rest, fontSize: .title3)
                             Spacer()
                         }
-                        
-                        
+
+
                         // MARK: restaurant description
                         // need add read more
                         Text(rest.description)
@@ -74,21 +135,6 @@ struct RestaurantDetailView: View {
 
             }
         }
-//        .simultaneousGesture(
-//           DragGesture()
-//            .onChanged { value in
-//              if value.translation.height > 0 {
-//                 print("Scroll down")
-////                  imageScale += 0.1
-//                  imageScale = value.translation.height
-//
-//              }
-//               else {
-//                   imageScale = 1.0
-//               }
-//           }
-//
-//        )
         .ignoresSafeArea()
     }
 }
